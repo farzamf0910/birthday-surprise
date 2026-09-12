8a82ca2bde1003d775f521d9045e024b24d472c1
farzamf0910 <farza.mf09@gmail.com>
2026-09-12 22:56:16 +0530

    script.js

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function showMessage() {
       "One little secret before you go… 🤫❤️This year, I'm celebrating your birthday from here.But maybe next year, we won't need screens, calls, or distance.We'll be there, together, making the memories instead of just talking about them. ✨Until then, keep this little wish safe in your heart.Our next birthday chapter is waiting for us. 💫You deserve all the happiness in the world. ❤️✨ Love u bubblu💖" 
 
 }
+function unlockWebsite() {
+
+  const password = document.getElementById("passwordInput").value;
+
+  const correctPassword = "yourpassword";
+
+  if (password === correctPassword) {
+
+    document.getElementById("passwordScreen").style.display = "none";
+
+  } else {
+
+    document.getElementById("wrongPassword").innerText =
+      "Oops... that's not the secret password 💜";
+  }
+}
