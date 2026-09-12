7c3c58785fd0c6470b0a74c8b3fc1485c389b4bd
farzamf0910 <farza.mf09@gmail.com>
2026-09-12 21:28:13 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function openSurprise() {
+
+    document.querySelector(".welcome").style.display = "none";
+
+    document.getElementById("surprise").style.display = "block";
+
+}
+
+function showMessage() {
+
+    document.getElementById("message").innerHTML =
+      "One little secret before you go… 🤫❤️This year, I'm celebrating your birthday from here.But maybe next year, we won't need screens, calls, or distance.We'll be there, together, making the memories instead of just talking about them. ✨Until then, keep this little wish safe in your heart.Our next birthday chapter is waiting for us. 💫You deserve all the happiness in the world. ❤️✨ Love u bubblu💖" 
+
+}
